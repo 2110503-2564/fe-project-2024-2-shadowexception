b35ad66fc804f37aec4368d885e538ffd60d438d
Tananan Narkchuay <zackalocloud@gmail.com>
2025-03-25 21:40:28 +0700

    push to vercel

--- src/components/BookingList.tsx
+++ src/components/BookingList.tsx
@@ -1,4 +1,3 @@
-
 import BookingListClient from "./BookingListClient";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { getServerSession } from "next-auth/next";
@@ -7,19 +6,25 @@ import { Suspense } from "react";
 import { LinearProgress } from "@mui/material";
 
 export default async function BookingList() {
-    const session = await getServerSession(authOptions);
-    if (!session || !session.user.token) return null;
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user.token) return null;
 
-    const bookingItems = await getReservations(session.user.token.toString());
+  const bookingItems = await getReservations(session.user.token.toString());
 
-    return (
-        <>
-            <Suspense fallback={<p>Loading . . . <LinearProgress/></p>}>
-                <BookingListClient
-                    token={session.user.token.toString()} 
-                    bookings={bookingItems.data}
-                />
-            </Suspense>
-        </>
-    );
+  return (
+    <>
+      <Suspense
+        fallback={
+          <p>
+            Loading . . . <LinearProgress />
+          </p>
+        }
+      >
+        <BookingListClient
+          token={session.user.token.toString()}
+          bookings={bookingItems.data}
+        />
+      </Suspense>
+    </>
+  );
 }
--- src/libs/getUserProfile.tsx
+++ src/libs/getUserProfile.tsx
@@ -1,14 +1,17 @@
-export default async function getUserProfile( token: string ) {
-    const response = await fetch("https://project-sd-fronted-sub-backend.vercel.app/api/v1/auth/me", {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    });
-
-    if (!response.ok) {
-        throw new Error("Something went wrong in getting user profile");
+export default async function getUserProfile(token: string) {
+  const response = await fetch(
+    "https://project-sd-fronted-sub-backend.vercel.app/api/v1/auth/me",
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     }
-    
-    return await response.json();
+  );
+
+  if (!response.ok) {
+    throw new Error("Something went wrong in getting user profile");
+  }
+
+  return await response.json();
 }
