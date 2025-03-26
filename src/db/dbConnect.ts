a4d72598171b72be0085d242a833d1d0b0d1bb78
Rattapoom Phonyiam <lnwextra2@gamil.com>
2025-03-26 14:53:52 +0700

    gg

--- src/db/dbConnect.ts
+++ src/db/dbConnect.ts
@@ -10,7 +10,7 @@ export const dbConnect = async () => {
         return;
     }
 
-    const MONGODB_URI = process.env.MONGO_URI;
+    const MONGODB_URI = process.env.MONGODB_URI;
     if (!MONGODB_URI) throw new Error('MONGODB_URI is not defined');
     
     try {
