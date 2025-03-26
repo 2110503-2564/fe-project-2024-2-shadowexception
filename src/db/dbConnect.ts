10989b512247c6757472da3a31453ff7a79d6a94
Tananan Narkchuay <zackalocloud@gmail.com>
2025-03-26 14:57:33 +0700

    fix mongo

--- src/db/dbConnect.ts
+++ src/db/dbConnect.ts
@@ -3,21 +3,21 @@ import mongoose from "mongoose";
 let isConnected = false;
 
 export const dbConnect = async () => {
-    mongoose.set('strictQuery', true);
+  mongoose.set("strictQuery", true);
 
-    if (isConnected) {
-        console.log('MongoDB is already connected');
-        return;
-    }
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
 
-    const MONGODB_URI = process.env.MONGODB_URI;
-    if (!MONGODB_URI) throw new Error('MONGODB_URI is not defined');
-    
-    try {
-        await mongoose.connect(MONGODB_URI, {bufferCommands: false})
-        isConnected = true;
-        console.log('MongoDB connected');
-    }catch(error) {
-        console.log(error);
-    }
-}
+  const MONGODB_URI = process.env.MONGODB_URI;
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is not defined");
+
+  try {
+    await mongoose.connect(MONGODB_URI, { bufferCommands: false });
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
